01944ef5dfbb013fccf5227c2052837413a01ba1
Doğukan Şensezer <54752508+dknSEN@users.noreply.github.com>
2021-01-30 23:35:35 +0300

    Add files via upload

---
+++ Information.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfterFormat
+{
+    class Information
+    {
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPhysicallyInstalledSystemMemory(out long TotalMemoryInKilobytes);
+        public static PerformanceCounter theCPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        public static PerformanceCounter theRAMCounter = new PerformanceCounter("Memory", "Available MBytes");
+        public static ManagementObjectSearcher myGPU = new ManagementObjectSearcher("select * from Win32_VideoController");
+        public static ManagementObjectSearcher myCPU = new ManagementObjectSearcher("select * from Win32_Processor");
+        public static ManagementObjectSearcher myOperatingSystem = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AfterFormat
+{
+    public class Settings
+    {
+        public static void SetLocalMachineRegistry(string code, string name, string value)
+        {
+            using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            using (var key = hklm.OpenSubKey(code, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(name, value);
+                key.Close();
+            }
+        }
+        public static void SetCurrentUserRegistry(string code, string name, string value)
+        {
+            using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+            using (var key = hklm.OpenSubKey(code, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(name, value);
+                key.Close();
+            }
+        }
+        public static void SetUsersRegistry(string code, string name, string value)
+        {
+            using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.Users, RegistryView.Registry64))
+            using (var key = hklm.OpenSubKey(code, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(name, value);
+                key.Close();
+            }
+        }
+        public static void SetClassesRootRegistry(string code, string name, string value)
+        {
+            using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.Users, RegistryView.Registry64))
+            using (var key = hklm.OpenSubKey(code, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(name, value);
+                key.Close();
+            }
+        }
+        public static void SetCurrentConfigRegistry(string code, string name, string value)
+        {
+            using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.Users, RegistryView.Registry64))
+            using (var key = hklm.OpenSubKey(code, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(name, value);
+                key.Close();
+            }
+        }
+        public static void ExecuteShellCommand(string arg)
+        {
+            Process pr = new Process();
+            pr.StartInfo.FileName = "cmd.exe";
+            pr.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            pr.StartInfo.Arguments = arg;
+            pr.Start();
+        }
+    }
+}
